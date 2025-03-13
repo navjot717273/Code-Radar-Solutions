f2dd1c1e5c4ba04a46aa0bcd9edc45c1830a827d
Navjot Singh <navjot717273@gmail.com>
2025-03-13 16:34:45 +0530

    Solution to Find the Most Frequent Character

--- Find the Most Frequent Character@@676c039d80e1714600f67647/code.c
+++ Find the Most Frequent Character@@676c039d80e1714600f67647/code.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 int main() {
     char a[200];
     scanf("%[^\n]%*c",&a);
     int n=strlen(a);
     int max=0;
-    char c[1];
+    char c;
     for(int s=0;s<n;s++){
         int k=0;
         for(int j=0;j<n;j++){
@@ -18,9 +19,9 @@ int main() {
         }
         if(k>max){
             max=k;
-            c[0]=a[s];
+            c=a[s];
         }
     }
-    printf("%d",max);
+    printf("%s",c);
 return 0;
 }
