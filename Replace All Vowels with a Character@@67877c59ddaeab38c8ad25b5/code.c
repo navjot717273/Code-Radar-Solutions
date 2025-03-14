d9d493b7dddd206a746e88cdc1f5454584ffc5c0
Navjot Singh <navjot717273@gmail.com>
2025-03-14 21:20:16 +0530

    Solution to Replace All Vowels with a Character

--- Replace All Vowels with a Character@@67877c59ddaeab38c8ad25b5/code.c
+++ Replace All Vowels with a Character@@67877c59ddaeab38c8ad25b5/code.c
@@ -5,9 +5,11 @@ int main() {
     char a[200];
     scanf("%[^\n]%*c",&a);
     int n=strlen(a);
+    char b;
+    scanf("%c",&b);
     for(int s=0;s<n;s++){
         if(a[s]=='a' || a[s]=='e' || a[s]=='i' || a[s]=='o' || a[s]=='u'){
-            a[s]='*';
+            a[s]=b;
         }
     }
     printf("%s",a);
