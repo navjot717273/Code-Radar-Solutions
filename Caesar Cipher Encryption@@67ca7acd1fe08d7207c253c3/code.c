5dc89689d1d3702894b1ac991e0fb07d41905356
Navjot Singh <navjot717273@gmail.com>
2025-03-15 12:17:17 +0530

    Solution to Caesar Cipher Encryption

---
+++ Caesar Cipher Encryption@@67ca7acd1fe08d7207c253c3/code.c
@@ -0,0 +1,22 @@
+void caesarCipher(char message[],int shift,char encrypted[]){
+    int n=strlen(message);
+    int k=0;
+    for(int s=0;s<n;s++){
+        if(message[s]=='z'){
+            encrypted[k]='a';
+            k++;
+        }
+        else if(message[s]=='Z'){
+            encrypted[k]='A';
+            k++;
+        }
+        else if((message[s]>='a' && message[s]<'z') || (message[s]>='A' && message[s]<'Z')){
+            encrypted[k]=message[s]+shift;
+            k++;
+        }
+        else{
+            encrypted[k]=message[s];
+            k++;
+        }
+    }
+}
