a721c7d814a874527e9d7d7c3575d6b8d61bf9af
Navjot Singh <navjot717273@gmail.com>
2025-03-21 18:01:13 +0530

    Solution to Movie Ticket Booking System

---
+++ Movie Ticket Booking System@@67877d61dc107486ad5c2751/code.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+struct Student{
+    char roll[100];
+    char name[99];
+    float marks;    
+};
+int main() {
+    int n;
+    scanf("%d",&n);
+    struct Student students[n];
+    float car,truck,bike=0;
+    for(int i =0;i<n;i++){
+        scanf("%s %s %f",&students[i].roll,&students[i].name,&students[i].marks);
+        if(students[i].name[0]=='C'){
+            car=car+students[i].marks;
+        }
+        else if(students[i].name[0]=='T'){
+            truck=truck+students[i].marks;
+        }
+        else if(students[i].name[0]=='B'){
+            bike=bike+students[i].marks;
+        }
+    }
+    printf("Standard: %.2f, Premium: %.2f, VIP: %.2f",car,truck,bike);
+}
