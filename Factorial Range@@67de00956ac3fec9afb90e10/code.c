db1a128475f96722a442a84c60fc97f27ca16cf7
Navjot Singh <navjot717273@gmail.com>
2025-03-31 22:57:49 +0530

    Solution to Factorial Range

--- Factorial Range@@67de00956ac3fec9afb90e10/code.c
+++ Factorial Range@@67de00956ac3fec9afb90e10/code.c
@@ -4,15 +4,18 @@ int factorialRange(int s,int e){
         sum=1;
         if(m==0){
             printf("1");
+            printf("\n");
         }
         else if(m==1){
             printf("1");
+            printf("\n");
         }
         else{
             for(int n=1;n<=m;n++){
                 sum=sum+n;
             }
             printf("%d",sum);
+            printf("\n");
         }
     }
 }
